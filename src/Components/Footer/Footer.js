b7ca6df1173fa9bb2a24e60da2300100f8ab47e3
Manu López <manueellg97@gmail.com>
2022-09-06 18:17:47 +0200

    Hola Mundo

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,30 @@
+function formataboutus(text) {
+    return text.presentacion + ' ' + text.lastName;
+  }
+
+  
+  
+  function Footer() {
+    const text = {
+      presentacion: 'Somos un grupo de programadores llamados ',
+      lastName: 'Manuel López, Aitor y Rodolfo'
+    };
+
+    const element = (
+      <div>
+    Llama al 666 para cualquier duda
+      </div>
+    );
+    
+    return (
+      <div className="Footer">       
+        <h1>Hola, {formataboutus(text)}</h1>
+        <div>{element}</div>
+      </div>
+    );
+  }
+  
+  
+  
+  
+  export default Footer;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+function puntosporjornada(number) {
+
+}
+
+function Header() {
+    const jugadora = {
+        Nombre: 'Manuel López',
+        Equipo: 'Real Madrid'
+    };
+}
